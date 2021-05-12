b2a0c79659ca453e6db74e4b9fdfed262135a7b8
Pravin Ade <prav.ade17@gmail.com>
2021-05-12 07:09:50 +0530

    Bank Managment system develop in python

---
+++ bank managment system.py
@@ -0,0 +1,52 @@
+# Mini project
+# Banking application
+"""
+i used Hierarchical inheritance to develop mini bank project
+ Hierarchical inheritance - means inheriting properties from one class to multiple class
+ which are present in same level
+"""
+class account:
+    """ static variable - which is not vary object to object"""
+    bank_name = "ADE Bank, Vitthalwadi"
+    def __init__(self, name, balance, min_balance):
+        self.name = name
+        self.balance = balance
+        self.min_balance = min_balance
+
+    def deposit(self, amount): # instance method
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance-amount >= self.min_balance:
+            self.balance -= amount
+        else:
+            print("Sorry,Insufficient Funds")
+
+    def printStatement(self):
+        print("Account Balance:", self.balance)
+
+class current(account):
+    def __init__(self, name, balance):
+        super().__init__(name, balance, min_balance = -1000)
+    def __str__(self):
+        return "{}'s Current Account with Balance:{}".format(self.name, self.balance, self.min_balance)
+
+class saving(account):
+    def __init__(self, name, balance):
+        super().__init__(name, balance, min_balance=0)
+    def __str__(self):
+        return "{}'s saving account with Balance:{}".format(self.name, self.balance, self.min_balance)
+
+print("Welcome to", account.bank_name)
+name = input("Enter Name:")
+balance = float(input("Enter Balance:"))
+c = saving(name, balance)
+print(c)
+c.deposit(5000)
+c.printStatement()
+c.withdraw(16000)
+c.withdraw(15000)
+print(c)
+
+
+
